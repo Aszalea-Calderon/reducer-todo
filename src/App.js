f6894a1c23cf5b524cce9d2630bbaad2f21556eb
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-01-27 19:45:54 -0800

    in process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useReducer, useState } from "react";
 import "./App.css";
+import { initialState } from "./components/initalState";
+import toDoReducer from "./reducers/toDoReducer";
+import ToDo from "./components/Todo";
 
 function App() {
+  const [state, dispatch] = useReducer(toDoReducer, initialState);
+
+  //Application level State
+  const [newToDo, setNewToDo] = useState({
+    todo: "a",
+  });
+
+  // const [editing, setEditing] = useState(false);
+  // const [clear, setClear] = useState("");
+
+  const handleChanges = (e) => {
+    setNewToDo(e.target.value);
+  };
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>ToDo</h1>
+      <input
+        type="text"
+        // value={newToDo}
+        onChange={handleChanges}
+        name="toDo"
+        placeholder="What do you need to do today?"
+      />
+      <button>Add to do</button>
+      {/* //Todo's -- Edit, Delete*/}
+      <ToDo />
     </div>
   );
 }
---
+++ src/reducers/toDoReducer.jsx
@@ -0,0 +1,4 @@
+const toDoReducer = (state, action) =>{
+  return state;
+}
+export default toDoReducer
